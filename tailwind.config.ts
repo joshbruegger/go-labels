d1a7bb21337a8642632477623c40f38a7436574b
Josh Bruegger <josh@bruegger.it>
2024-12-24 17:12:31 +0100

    style: update theme colors and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
 import type { Config } from 'tailwindcss';
 import tailwindcssAnimate from 'tailwindcss-animate';
-import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config: Config = {
 	darkMode: ['class'],
@@ -66,8 +65,22 @@ const config: Config = {
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
 			},
+			fontSize: {
+				sm: '0.750rem',
+				base: '1rem',
+				xl: '1.333rem',
+				'2xl': '1.777rem',
+				'3xl': '2.369rem',
+				'4xl': '3.158rem',
+				'5xl': '4.210rem'
+			},
 			fontFamily: {
-				sans: [...fontFamily.sans]
+				heading: 'Arimo',
+				body: 'Open Sans'
+			},
+			fontWeight: {
+				normal: '400',
+				bold: '700'
 			},
 			keyframes: {
 				'accordion-down': {
