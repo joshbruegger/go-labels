613dad91ae5143b44b6d12aaaf6ca0c0e9953d1b
Josh Bruegger <josh@bruegger.it>
2025-01-29 22:45:57 +0100

    refactor: delete outddated unused files

--- src/routes/(protected)/questionnaire/schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const responseSchema = z.object({
-	submission_id: z.string(),
-	question_id: z.string(),
-	choice_id: z.string()
-});
-
-export type ResponseSchema = typeof responseSchema;
